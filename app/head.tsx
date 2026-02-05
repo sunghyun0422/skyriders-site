106269835d0d89a19c744f6acd7818db48adab8d
Sung Hyun <ksh020422@naver.com>
2026-02-05 17:09:21 +0900

    Add Google Search Console verification meta tag

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <meta
+        name="google-site-verification"
+        content="JQTZUju9TUFmpeW2Q7Q2Vtfy6bhko-MyAtscctjwe9w"
+      />
+    </>
+  );
+}
